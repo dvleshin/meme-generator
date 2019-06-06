24dc8fdbc2fc27351d3e7f8739d92632f1643ef6
Dima Leshin <dmitryleshin@gmail.com>
2019-06-06 18:12:46 +0300

    added responsivity to the index page

--- js/meme-service.js
+++ js/meme-service.js
@@ -11,11 +11,43 @@ let gImgs = [{
     {
         id: 2,
         url: 'img/2.jpg',
-        keywords: ['man', 'trump', 'angry'],
+        keywords: ['child', 'cunning'],
     }, {
         id: 3,
         url: 'img/3.jpg',
-        keywords: ['animals', 'puppies', 'love'],
+        keywords: ['man', 'smart'],
+    }, {
+        id: 4,
+        url: 'img/4.jpg',
+        keywords: ['man', 'funny'],
+    }, {
+        id: 5,
+        url: 'img/5.jpg',
+        keywords: ['obama', 'man', 'laughs'],
+    }, {
+        id: 6,
+        url: 'img/6.jpg',
+        keywords: ['men', 'kiss', 'funny'],
+    }, {
+        id: 7,
+        url: 'img/7.jpg',
+        keywords: ['children', 'dance', 'fun'],
+    }, {
+        id: 8,
+        url: 'img/8.jpg',
+        keywords: ['tramp', 'man', 'angry'],
+    }, {
+        id: 9,
+        url: 'img/9.jpg',
+        keywords: ['child', 'black', 'surprised'],
+    }, {
+        id: 10,
+        url: 'img/10.jpg',
+        keywords: ['dog', 'stretching', 'fun'],
+    }, {
+        id: 11,
+        url: 'img/11.jpg',
+        keywords: ['man', 'cheers', 'dicaprio'],
     }
 ];
 
