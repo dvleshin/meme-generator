d8f80e52df16cec355584d7130d2fe3c96bf6d69
Dima Leshin <dmitryleshin@gmail.com>
2019-06-06 18:33:59 +0300

    css changes

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -10,13 +10,12 @@ function renderGallery() {
 
     var strHtmls = imgs.map(img => {
         return `
-        <section class="cards">
             <article>
             <a href="editor.html" onclick="imgPicked(${img.id})"><img src="${img.url}" class="article-img"></a>
             </article>
-        </section>  `
+            `
     })
-    document.querySelector('.cards').innerHTML = strHtmls.join('');
+    document.querySelector('.memes-imgs').innerHTML = strHtmls.join('');
 }
 
 function imgPicked(id) {
